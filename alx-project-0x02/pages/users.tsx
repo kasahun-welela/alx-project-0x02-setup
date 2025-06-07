340d755030a0a834b774b22387341addb02625ce
kasahun-welela <kasahunwelela1@gmail.com>
2025-06-08 00:07:07 +0300

    update the name

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -30,8 +30,8 @@ const UsersPage: React.FC<{ users: UserProps[] }> = ({ users }) => {
 
 export default UsersPage;
 
-export const getStaticProps = async () => {
+export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await response.json();
   return { props: { users: data } };
-};
+}
