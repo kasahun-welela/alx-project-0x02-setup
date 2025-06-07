4cf09f78ee8490003a25b71540636989cd29eeac
kasahun-welela <kasahunwelela1@gmail.com>
2025-06-08 00:04:12 +0300

    update the name according to checker

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -27,8 +27,8 @@ const PostsPage: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
 
 export default PostsPage;
 
-export const getStaticProps = async () => {
+export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await response.json();
   return { props: { posts: data } };
-};
+}
